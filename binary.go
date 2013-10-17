93bbdbcaf3e85733df1d2e65451229b1dd38b936
Jonathan Pulfer <JonPulfer@server.fake>
2013-10-17 09:21:19 +0100

    Added a quadSplit function for the byte based methods.

--- binary.go
+++ binary.go
@@ -1,6 +1,9 @@
 package serpent
 
+type uint32le [4]byte
 type uint128le [16]byte
+type uint256le [32]byte
+type bitsliceNew []uint32le
 
 const (
 	digits = "0123456789abcdefghijklmnopqrstuvwxyz"
@@ -14,6 +17,7 @@ var shifts = [len(digits) + 1]uint{
 	1 << 5: 5,
 }
 
+// Function newFromUint64 creates a uint128le from uint64 'num'.
 func newFromUint64(num uint64) (result uint128le) {
 	for i := 0; i < 8; i++ {
 		result[i] = byte(num & 0xff)
@@ -22,21 +26,44 @@ func newFromUint64(num uint64) (result uint128le) {
 	return result
 }
 
-func (num *uint128le) binaryXor(other uint128le) {
+// Function newUint32le takes a []byte and returns a uint32le.
+func newUint32le(num []byte) uint32le {
+	var result uint32le
+	for i, _ := range num {
+		result[i] = num[i]
+	}
+
+	return result
+}
+
+// Function xor performs a sequential Xor operation on the elements
+// in 'args'.
+func xorNEW(args []uint128le) *uint128le {
+	result := &args[0]
+	for i := 1; i < len(args); i++ {
+		result.binaryXorNEW(args[i])
+	}
+	return result
+}
+
+// Method quadSplit splits a 128-bit bitstring into a list of 4 32-bit
+// bitstrings.
+func (num uint128le) quadSplitNEW() []uint32le {
+	result := bitsliceNew{newUint32le(num[0:3]),
+		newUint32le(num[4:7]),
+		newUint32le(num[8:11]),
+		newUint32le(num[12:15])}
+	return result
+}
+
+// Method binaryXor performs a Xor operation between 'num' and 'other'.
+func (num *uint128le) binaryXorNEW(other uint128le) {
 	for i := 0; i < 16; i++ {
 		num[i] ^= other[i]
 	}
 }
 
-func xor(args []uint128le) *uint128le {
-	result := &args[0]
-	for i := 1; i < len(args); i++ {
-		result.binaryXor(args[i])
-	}
-	return result
-}
-
-// Return 'num' as a string using shifts
+// Method String returns 'num' as a string using shifts and a mask
 func (num uint128le) String() string {
 	b := uint64(2)
 	s := shifts[2]
